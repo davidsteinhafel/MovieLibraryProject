158952224aaa0b5520ee7ffd4ede0e33d099e6a2
David steinhafel <davidsteinhafel@outlook.com>
2020-03-02 11:28:42 -0600

    Added genre property to our movie model. Seeded db with given data.

---
+++ WebAPISample/Data/ApplicationContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPISample.Models;
+
+namespace WebAPISample.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Seed data - needs migration
+            // modelBuilder.Entity<Movie>
+            //  .HasData(
+            //  new Movie{Fill All Properties}
+            //  );
+            // View PlayerTracker project for example
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Movie>()
+                .HasData(
+               new Movie { Title = "The Departed", Genre = "Drama", Director = "Martin Scorsese" },
+               new Movie { Title = "The Dark Knight", Genre = "Drama", Director = "Christopher Nolan" },
+               new Movie { Title = "Inception", Genre = "Drama", Director = "Christopher Nolan" },
+               new Movie { Title = "Pineapple Express", Genre = "Comedy", Director = "David Gordon Green" },
+               new Movie { Title = "Die Hard", Genre = "Action", Director = "John McTiernan" });
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
