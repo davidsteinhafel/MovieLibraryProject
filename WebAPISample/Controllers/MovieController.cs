ea6132c6c40d66825813e5bb50e2d9ae96445c6b
David steinhafel <davidsteinhafel@outlook.com>
2020-03-02 15:28:04 -0600

    added post, put, and delete methods for our movie controller

--- WebAPISample/Controllers/MovieController.cs
+++ WebAPISample/Controllers/MovieController.cs
@@ -20,39 +20,49 @@ namespace WebAPISample.Controllers
         }
         // GET api/movie
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
             // Retrieve all movies from db logic
-            //return _context.Movies.Include(m => m.Title == ).Select();
-            return new string[] { "movie1 string", "movie2 string" };
+            var movie = _context.Movies;
+            if (movie == null)
+            {
+                return BadRequest();
+            }
+            return Ok(_context.Movies);
         }
 
         // GET api/movie/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult> Get(int id)
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
         {
-            var movieOnDb = await _context.Movies.Include(x => x.MovieId == id).FirstOrDefaultAsync();
+            var movieOnDb = await _context.Movies.Where(x => x.MovieId == id).FirstOrDefaultAsync();
             if (movieOnDb == null)
             {
                 return NotFound();
             }
             return Ok(movieOnDb);
         }
-
+        
         // POST api/movie
         [HttpPost]
         public void Post([FromBody]Movie value)
         {
             // Create movie in db logic
-
-
+            _context.Movies.Add(value);
+            _context.SaveChanges();
         }
 
         // PUT api/movie/5
         [HttpPut]
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]Movie movie)
         {
             // Update movie in db logic
+            var movieOnDb = _context.Movies.Single(x => x.MovieId == id);
+         
+            movieOnDb.Director = movie.Director;
+            movieOnDb.Genre = movie.Genre;
+            movieOnDb.Title = movie.Title;
+            _context.SaveChanges();
         }
 
         // DELETE api/movie/5
@@ -60,6 +70,10 @@ namespace WebAPISample.Controllers
         public void Delete(int id)
         {
             // Delete movie from db logic
+            var movieOnDb = _context.Movies.Single(x => x.MovieId == id);
+            _context.Movies.Remove(movieOnDb);
+            _context.SaveChanges();
+
         }
     }
 }
